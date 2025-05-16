66034a26506354a3abca4f882a89600807c1707c
ArtemKolomeichuk <pupkin2281234@gmail.com>
2025-05-16 18:15:02 +0300

    dsdsdsds
    asa

---
+++ vodoshobzhenie2.cpp
@@ -0,0 +1,10 @@
+﻿// vodoshobzhenie2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Hello World!";
+}
+
